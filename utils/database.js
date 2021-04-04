1cd6d0e61e026640960713c8f615334636b94d3a
Ankit Kumar Sharma <ankit.s@purplle.com>
2021-04-04 17:48:44 +0530

    Added db file to connect to mongodb instance

---
+++ utils/database.js
@@ -0,0 +1,29 @@
+const mongodb = require("mongodb");
+const mongoClient = mongodb.MongoClient;
+const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.6gyc1.mongodb.net/fakify_api?retryWrites=true&w=majority`;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  mongoClient
+    .connect(URI)
+    .then((client) => {
+      _db = client.db();
+      console.log("Connected!");
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
