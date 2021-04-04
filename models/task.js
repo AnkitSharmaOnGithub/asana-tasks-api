c3c9c15c80799a99bfa465ec9d3df315afe558f8
Ankit Kumar Sharma <ankit.s@purplle.com>
2021-04-04 19:31:16 +0530

    Added the task model

---
+++ models/task.js
@@ -0,0 +1,51 @@
+const getDb = require("../utils/database").getDb;
+const collection = "tasks";
+
+/* 
+    Properties => Values they can have
+    ______________________________________________________
+
+    
+
+*/
+
+class Task {
+  constructor(
+    title,
+    priority,
+    deadline,
+    section,
+    description,
+    comments,
+    tags,
+    status
+  ) {
+    this.title = title;
+    this.priority = priority;
+    this.deadline = deadline;
+    this.section = section;
+    this.description = description;
+    this.comments = comments;
+    this.tags = tags;
+    this.status = status;
+  }
+
+  save() {
+    let db = getDb();
+    return db.collection(collection).insertOne({
+      title: this.title,
+      priority: this.priority,
+      deadline: this.deadline,
+      section: this.section,
+      description: this.description,
+      comments: this.comments,
+      tags: this.tags,
+      status: this.status,
+    });
+  }
+
+  static fetchTasks() {
+    //   Fetch tasks only that have status => Not completed
+    return db.collection(collection).find({}).toArray();
+  }
+}
