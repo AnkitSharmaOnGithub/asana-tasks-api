b4628f35b3bcf6aa47c9d63e2224d28b93fbced8
Ankit Kumar Sharma <ankit.s@purplle.com>
2021-04-04 22:17:13 +0530

    Update

--- controllers/task.js
+++ controllers/task.js
@@ -1,7 +1,7 @@
 const Task = require("../models/task");
 
 exports.getTasks = async (req, res, next) => {
-  const tasks = await Task.getTasks();
+  const tasks = await Task.fetchTasks();
 
   res.json({
     message: "Tasks Fetched",
