cde8ca99bfd249d92ce6ffb47d844a3f2cf0035c
Ankit Kumar Sharma <ankit.s@purplle.com>
2021-04-05 09:47:01 +0530

    Added routes and database connect functionality to the app

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 const taskRoutes = require("./routes/tasks");
+const mongoConnect = require("./utils/database").mongoConnect;
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(taskRoutes);
+app.use("/tasks", taskRoutes);
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
@@ -14,4 +15,6 @@ app.use((error, req, res, next) => {
   const data = error.data;
   res.status(status).json({ message: message, data: data });
 });
-app.listen(8000);
+mongoConnect(() => {
+  app.listen(8000);
+});
